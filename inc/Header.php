3b72a87fe3f7e54edae7e1e00f2a53e23520e192
Myron <myronmax_seelen@live.nl>
2019-05-06 21:08:01 +0200

    Header Commit

---
+++ inc/Header.php
@@ -0,0 +1,21 @@
+
+<link rel="stylesheet" as="style" type="text/css" href="/css/style.css">
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" id="ToernooiText" href="Index.php">Toernooi</a>
+        <button class="navbar-toggler" type="button"data-toggle="collapse" data-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto" id="NavBarText">
+                <li class="nav-item">
+                    <a class="nav-link" id="NavBarTextSpelersToevoegen" href="./Geschiedenis.php">Spelers toevoegen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="NavBarTextToernooiOnderhoud" href="./Fotos.php">Toernooi Onderhoud</a>
+                </li>
+        </div>
+    </nav>
+</header>
