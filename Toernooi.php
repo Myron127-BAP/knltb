c0d65981886e8ab9cc5dc585df85f8e4b1450e9e
Myron <myronmax_seelen@live.nl>
2019-05-07 23:18:52 +0200

    Toernooi randomizer

--- Toernooi.php
+++ Toernooi.php
@@ -3,61 +3,162 @@
 
     include_once './inc/Database.inc.php';
 
-        $id1 = rand(1, 8);
-        $id2 = rand(1, 8);
-        $id3 = rand(1, 8);
-        $id4 = rand(1, 8);
-        $id5 = rand(1, 8);
-        $id6 = rand(1, 8);
-
-
-
+        $id1  = rand(1, 16);
+        $id2  = rand(1, 16);
+        $id3  = rand(1, 16);
+        $id4  = rand(1, 16);
+        $id5  = rand(1, 16);
+        $id6  = rand(1, 16);
+        $id7  = rand(1, 16);
+        $id8  = rand(1, 16);
+        $id9  = rand(1, 16);
+        $id10 = rand(1, 16);
+        $id11 = rand(1, 16);
+        $id12 = rand(1, 16);
+        $id13 = rand(1, 16);
+        $id14 = rand(1, 16);
+        $id15 = rand(1, 16);
+        $id16 = rand(1, 16);
 
         /**Random Cijfer 1 $id1**/
-        if ($id1 == $id2){$id1 = rand(1, 8);}
-        if ($id1 == $id3){$id1 = rand(1, 8);}
-        if ($id1 == $id4){$id1 = rand(1, 8);}
-        if ($id4 == $id1){$id4 = rand(1, 8);}
-        if ($id1 == $id5){$id1 = rand(1, 8);}
-        if ($id1 == $id6){$id1 = rand(1, 8);}
-
-        /**Random Cijfer 2 $id2**/
-        if ($id2 == $id3){$id2 = rand(1, 8);}
-        if ($id2 == $id4){$id2 = rand(1, 8);}
-        if ($id2 == $id5){$id2 = rand(1, 8);}
-        if ($id2 == $id6){$id2 = rand(1, 8);}
-
+        if ($id1 == $id2) {$id1 = rand(1, 16);}     /**Random Cijfer 2 $id2**/
+        if ($id1 == $id3) {$id1 = rand(1, 16);}     if ($id2 == $id3) {$id2 = rand(1, 16);}
+        if ($id1 == $id4) {$id1 = rand(1, 16);}     if ($id2 == $id4) {$id2 = rand(1, 16);}
+        if ($id1 == $id5) {$id1 = rand(1, 16);}     if ($id2 == $id5) {$id2 = rand(1, 16);}
+        if ($id1 == $id6) {$id1 = rand(1, 16);}     if ($id2 == $id6) {$id2 = rand(1, 16);}
+        if ($id1 == $id7) {$id1 = rand(1, 16);}     if ($id2 == $id7) {$id2 = rand(1, 16);}
+        if ($id1 == $id8) {$id1 = rand(1, 16);}     if ($id2 == $id8) {$id2 = rand(1, 16);}
+        if ($id1 == $id9) {$id1 = rand(1, 16);}     if ($id2 == $id9) {$id2 = rand(1, 16);}
+        if ($id1 == $id10){$id1 = rand(1, 16);}     if ($id2 == $id10){$id2 = rand(1, 16);}
+        if ($id1 == $id11){$id1 = rand(1, 16);}     if ($id2 == $id11){$id2 = rand(1, 16);}
+        if ($id1 == $id12){$id1 = rand(1, 16);}     if ($id2 == $id12){$id2 = rand(1, 16);}
+        if ($id1 == $id13){$id1 = rand(1, 16);}     if ($id2 == $id13){$id2 = rand(1, 16);}
+        if ($id1 == $id14){$id1 = rand(1, 16);}     if ($id2 == $id14){$id2 = rand(1, 16);}
+        if ($id1 == $id15){$id1 = rand(1, 16);}     if ($id2 == $id15){$id2 = rand(1, 16);}
+        if ($id1 == $id16){$id1 = rand(1, 16);}     if ($id2 == $id16){$id2 = rand(1, 16);}
         /**Random Cijfer 3 $id3**/
-        if ($id3 == $id4){$id3 = rand(1, 8);}
-        if ($id3 == $id5){$id3 = rand(1, 8);}
-        if ($id3 == $id6){$id3 = rand(1, 8);}
-
-        /**Random Cijfer 4 $id4**/
-        if ($id4 == $id5){$id4 = rand(1, 8);}
-        if ($id4 == $id6){$id4 = rand(1, 8);}
-
+        if ($id3 == $id4) {$id3 = rand(1, 16);}     /**Random Cijfer 4 $id4**/
+        if ($id3 == $id5) {$id3 = rand(1, 16);}     if ($id4 == $id5) {$id4 = rand(1, 16);}
+        if ($id3 == $id6) {$id3 = rand(1, 16);}     if ($id4 == $id6) {$id4 = rand(1, 16);}
+        if ($id3 == $id7) {$id3 = rand(1, 16);}     if ($id4 == $id7) {$id4 = rand(1, 16);}
+        if ($id3 == $id8) {$id3 = rand(1, 16);}     if ($id4 == $id8) {$id4 = rand(1, 16);}
+        if ($id3 == $id9) {$id3 = rand(1, 16);}     if ($id4 == $id9) {$id4 = rand(1, 16);}
+        if ($id3 == $id10){$id3 = rand(1, 16);}     if ($id4 == $id10){$id4 = rand(1, 16);}
+        if ($id3 == $id11){$id3 = rand(1, 16);}     if ($id4 == $id11){$id4 = rand(1, 16);}
+        if ($id3 == $id12){$id3 = rand(1, 16);}     if ($id4 == $id12){$id4 = rand(1, 16);}
+        if ($id3 == $id13){$id3 = rand(1, 16);}     if ($id4 == $id13){$id4 = rand(1, 16);}
+        if ($id3 == $id14){$id3 = rand(1, 16);}     if ($id4 == $id14){$id4 = rand(1, 16);}
+        if ($id3 == $id15){$id3 = rand(1, 16);}     if ($id4 == $id15){$id4 = rand(1, 16);}
+        if ($id3 == $id16){$id3 = rand(1, 16);}     if ($id4 == $id16){$id4 = rand(1, 16);}
         /**Random Cijfer 5 $id5**/
-        if ($id5 == $id6){$id5 = rand(1, 8);}
+        if ($id5 == $id6) {$id5 = rand(1, 16);}     /**Random Cijfer 6 $id6**/
+        if ($id5 == $id7) {$id5 = rand(1, 16);}     if ($id6 == $id7) {$id6 = rand(1, 16);}
+        if ($id5 == $id8) {$id5 = rand(1, 16);}     if ($id6 == $id8) {$id6 = rand(1, 16);}
+        if ($id5 == $id9) {$id5 = rand(1, 16);}     if ($id6 == $id9) {$id6 = rand(1, 16);}
+        if ($id5 == $id10){$id5 = rand(1, 16);}     if ($id6 == $id10){$id6 = rand(1, 16);}
+        if ($id5 == $id11){$id5 = rand(1, 16);}     if ($id6 == $id11){$id6 = rand(1, 16);}
+        if ($id5 == $id12){$id5 = rand(1, 16);}     if ($id6 == $id12){$id6 = rand(1, 16);}
+        if ($id5 == $id13){$id5 = rand(1, 16);}     if ($id6 == $id13){$id6 = rand(1, 16);}
+        if ($id5 == $id14){$id5 = rand(1, 16);}     if ($id6 == $id14){$id6 = rand(1, 16);}
+        if ($id5 == $id15){$id5 = rand(1, 16);}     if ($id6 == $id15){$id6 = rand(1, 16);}
+        if ($id5 == $id16){$id5 = rand(1, 16);}     if ($id6 == $id16){$id6 = rand(1, 16);}
+        /**Random Cijfer 7 $id7**/
+        if ($id7 == $id8) {$id7 = rand(1, 16);}     /**Random Cijfer 8 $id8**/
+        if ($id7 == $id9) {$id7 = rand(1, 16);}     if ($id8 == $id9) {$id8 = rand(1, 16);}
+        if ($id7 == $id10){$id7 = rand(1, 16);}     if ($id8 == $id10){$id8 = rand(1, 16);}
+        if ($id7 == $id11){$id7 = rand(1, 16);}     if ($id8 == $id11){$id8 = rand(1, 16);}
+        if ($id7 == $id12){$id7 = rand(1, 16);}     if ($id8 == $id12){$id8 = rand(1, 16);}
+        if ($id7 == $id13){$id7 = rand(1, 16);}     if ($id8 == $id13){$id8 = rand(1, 16);}
+        if ($id7 == $id14){$id7 = rand(1, 16);}     if ($id8 == $id14){$id8 = rand(1, 16);}
+        if ($id7 == $id15){$id7 = rand(1, 16);}     if ($id8 == $id15){$id8 = rand(1, 16);}
+        if ($id7 == $id16){$id7 = rand(1, 16);}     if ($id8 == $id16){$id8 = rand(1, 16);}
+        /**Random Cijfer 9 $id9**/
+        if ($id9 == $id10){$id9 = rand(1, 16);}     /**Random Cijfer 10 $id10**/
+        if ($id9 == $id11){$id9 = rand(1, 16);}     if ($id10 == $id11){$id10 = rand(1, 16);}
+        if ($id9 == $id12){$id9 = rand(1, 16);}     if ($id10 == $id12){$id10 = rand(1, 16);}
+        if ($id9 == $id13){$id9 = rand(1, 16);}     if ($id10 == $id13){$id10 = rand(1, 16);}
+        if ($id9 == $id14){$id9 = rand(1, 16);}     if ($id10 == $id14){$id10 = rand(1, 16);}
+        if ($id9 == $id15){$id9 = rand(1, 16);}     if ($id10 == $id15){$id10 = rand(1, 16);}
+        if ($id9 == $id16){$id9 = rand(1, 16);}     if ($id10 == $id16){$id10 = rand(1, 16);}
+        /**Random Cijfer 11 $id11**/
+        if ($id11 == $id12){$id11 = rand(1, 16);}    /**Random Cijfer 12 $id12**/
+        if ($id11 == $id13){$id11 = rand(1, 16);}    if ($id12 == $id13){$id12 = rand(1, 16);}
+        if ($id11 == $id14){$id11 = rand(1, 16);}    if ($id12 == $id14){$id12 = rand(1, 16);}
+        if ($id11 == $id15){$id11 = rand(1, 16);}    if ($id12 == $id15){$id12 = rand(1, 16);}
+        if ($id11 == $id16){$id11 = rand(1, 16);}    if ($id12 == $id16){$id12 = rand(1, 16);}
+        /**Random Cijfer 13 $id13**/
+        if ($id13 == $id14){$id13 = rand(1, 16);}    /**Random Cijfer 14 $id14**/
+        if ($id13 == $id15){$id13 = rand(1, 16);}    if ($id14 == $id15){$id14 = rand(1, 16);}
+        if ($id13 == $id16){$id13 = rand(1, 16);}    if ($id14 == $id16){$id14 = rand(1, 16);}
+        /**Random Cijfer 15 $id15**/
+        if ($id15 == $id16){$id15 = rand(1, 16);}
 
-        /**Radom Cijfer 1**/
+
+
+        /**Random Speler 1**/
         $randuser1 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id1'"; $Result1 = mysqli_query($conn, $randuser1);
         while($row1 = mysqli_fetch_assoc($Result1)){echo $_SESSION['speler1'] = $row1['voornaam'] . " " . $row1['achternaam'] . "<br/>";}
-        /**Radom Cijfer 2**/
+
+        /**Random Speler 2**/
         $randuser2 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id2'"; $Result2 = mysqli_query($conn, $randuser2);
         while($row2 = mysqli_fetch_assoc($Result2)){echo  $_SESSION['speler2'] = $row2['voornaam'] . " " . $row2['achternaam'] . "<br/>"; }
-        /**Radom Cijfer 3**/
+
+        /**Random Speler 3**/
         $randuser3 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id3'"; $Result3 = mysqli_query($conn, $randuser3);
         while($row3 = mysqli_fetch_assoc($Result3)){echo $_SESSION['speler3'] = $row3['voornaam'] . " " . $row3['achternaam'] . "<br/>";}
-        /**Radom Cijfer 4**/
+
+        /**Random Speler 4**/
         $randuser4 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id4'"; $Result4 = mysqli_query($conn, $randuser4);
         while($row4 = mysqli_fetch_assoc($Result4)){echo  $_SESSION['speler4'] = $row4['voornaam'] . " " . $row4['achternaam'] . "<br/>"; }
-        /**Radom Cijfer 5**/
+
+        /**Random Speler 5**/
         $randuser5 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id5'"; $Result5 = mysqli_query($conn, $randuser5);
         while($row5 = mysqli_fetch_assoc($Result5)){echo $_SESSION['speler5'] = $row5['voornaam'] . " " . $row5['achternaam'] . "<br/>";}
-        /**Radom Cijfer 6**/
+
+        /**Random Speler 6**/
         $randuser6 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id6'"; $Result6 = mysqli_query($conn, $randuser6);
         while($row6 = mysqli_fetch_assoc($Result6)){echo  $_SESSION['speler6'] = $row6['voornaam'] . " " . $row6['achternaam'] . "<br/>"; }
-        //Random Cijfer 7
+
+        //Random Speler 7
+        $randuser7 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id7'"; $Result7 = mysqli_query($conn, $randuser7);
+        while($row7 = mysqli_fetch_assoc($Result7)){echo  $_SESSION['speler7'] = $row7['voornaam'] . " " . $row7['achternaam'] . "<br/>"; }
+
+        //Random Speler 8
+        $randuser8 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id8'"; $Result8 = mysqli_query($conn, $randuser8);
+        while($row8 = mysqli_fetch_assoc($Result8)){echo  $_SESSION['speler8'] = $row8['voornaam'] . " " . $row8['achternaam'] . "<br/>"; }
+
+        //Random Speler 9
+        $randuser9 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id9'"; $Result9 = mysqli_query($conn, $randuser9);
+        while($row9 = mysqli_fetch_assoc($Result9)){echo  $_SESSION['speler9'] = $row9['voornaam'] . " " . $row9['achternaam'] . "<br/>"; }
+
+        //Random Speler 10
+        $randuser10 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id10'"; $Result10 = mysqli_query($conn, $randuser10);
+        while($row10 = mysqli_fetch_assoc($Result10)){echo  $_SESSION['speler10'] = $row10['voornaam'] . " " . $row10['achternaam'] . "<br/>"; }
+
+        //Random Speler 11
+        $randuser11 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id11'"; $Result11 = mysqli_query($conn, $randuser11);
+        while($row11 = mysqli_fetch_assoc($Result11)){echo  $_SESSION['speler11'] = $row11['voornaam'] . " " . $row11['achternaam'] . "<br/>"; }
+
+        //Random Speler 12
+        $randuser12 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id12'"; $Result12 = mysqli_query($conn, $randuser12);
+        while($row12 = mysqli_fetch_assoc($Result12)){echo  $_SESSION['speler12'] = $row12['voornaam'] . " " . $row12['achternaam'] . "<br/>"; }
+
+        //Random Speler 13
+        $randuser13 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id13'"; $Result13 = mysqli_query($conn, $randuser13);
+        while($row13 = mysqli_fetch_assoc($Result13)){echo  $_SESSION['speler13'] = $row13['voornaam'] . " " . $row13['achternaam'] . "<br/>"; }
+
+        //Random Speler 14
+        $randuser14 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id14'"; $Result14 = mysqli_query($conn, $randuser14);
+        while($row14 = mysqli_fetch_assoc($Result14)){echo  $_SESSION['speler14'] = $row14['voornaam'] . " " . $row14['achternaam'] . "<br/>"; }
+
+        //Random Speler 15
+        $randuser15 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id15'"; $Result15 = mysqli_query($conn, $randuser15);
+        while($row15 = mysqli_fetch_assoc($Result15)){echo  $_SESSION['speler15'] = $row15['voornaam'] . " " . $row15['achternaam'] . "<br/>"; }
+
+        //Random Speler 16
+        $randuser16 = "SELECT voornaam, achternaam FROM spelers WHERE id='$id16'"; $Result16 = mysqli_query($conn, $randuser16);
+        while($row16 = mysqli_fetch_assoc($Result16)){echo  $_SESSION['speler16'] = $row16['voornaam'] . " " . $row16['achternaam'] . "<br/>"; }
+
 
 
 
@@ -105,7 +206,7 @@
                     <li class="speler speler-bottom"><?= $_SESSION['speler6']?><span class="score"></span></li>
                 </ul>
                 <ul class="wedstrijd">
-                    <li class="speler speler-top"><?= $_SESSION['speler']?><span class="score"></span></li>
+                    <li class="speler speler-top"><?= $_SESSION['speler7']?><span class="score"></span></li>
                     <li class="speler speler-bottom"><?= $_SESSION['speler8']?><span class="score"></span></li>
                 </ul>
                 <ul class="wedstrijd">
